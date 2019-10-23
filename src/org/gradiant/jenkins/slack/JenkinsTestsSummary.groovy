1f8643db18c74f0854c10b284f991743c8a617fc
Aleksei Mordas <endy990@gmail.com>
2019-10-23 15:34:07 +0300

    Update JenkinsTestsSummary.groovy

--- src/org/gradiant/jenkins/slack/JenkinsTestsSummary.groovy
+++ src/org/gradiant/jenkins/slack/JenkinsTestsSummary.groovy
@@ -13,7 +13,7 @@ String getTestSummary() {
     def failed = testResultAction.getFailCount()
     def skipped = testResultAction.getSkipCount()
 
-    summary = "Test results:\n\t"
+    summary = "Test status:\n\t"
     summary = summary + ("Passed: " + (total - failed - skipped))
     summary = summary + (", Failed: " + failed)
     summary = summary + (", Skipped: " + skipped)
