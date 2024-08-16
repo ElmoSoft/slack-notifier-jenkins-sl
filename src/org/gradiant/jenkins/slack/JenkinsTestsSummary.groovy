ea5b076a522f4b3723b69ad83f68fabb7c467869
njelezny <nikolai.jelezny@elmosoft.net>
2024-08-16 09:47:48 +0300

    Added isQase flag to divide test results by 2

--- src/org/gradiant/jenkins/slack/JenkinsTestsSummary.groovy
+++ src/org/gradiant/jenkins/slack/JenkinsTestsSummary.groovy
@@ -4,19 +4,19 @@ import hudson.tasks.test.AbstractTestResultAction
 import hudson.model.Actionable
 
 @NonCPS
-String getTestSummary( titleMessage = "Test status:\n\t") {
+String getTestSummary(titleMessage = "Test status:\n\t", boolean isQase = false) {
   def testResultAction = currentBuild.rawBuild.getAction(AbstractTestResultAction.class)
   def summary = ""
 
   if (testResultAction != null) {
-    def total = testResultAction.getTotalCount()
-    def failed = testResultAction.getFailCount()
-    def skipped = testResultAction.getSkipCount()
+    def total = isQase ? testResultAction.getTotalCount() / 2 : testResultAction.getTotalCount()
+    def failed = isQase ? testResultAction.getFailCount() / 2 : testResultAction.getFailCount()
+    def skipped = isQase ? testResultAction.getSkipCount() / 2 : testResultAction.getSkipCount()
 
     summary = titleMessage
-    summary = summary + ("Passed: " + (total - failed - skipped))
-    summary = summary + (", Failed: " + failed)
-    summary = summary + (", Skipped: " + skipped)
+    summary += "Passed: " + (total - failed - skipped)
+    summary += ", Failed: " + failed
+    summary += ", Skipped: " + skipped
   } else {
     summary = "No tests found"
   }
